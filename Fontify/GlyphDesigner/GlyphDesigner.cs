b90c2fd8ab463cc68990e131a07ee9ff761e929c
dacucar <dacucar@dacucar.com>
2016-06-05 02:01:33 +0200

    Add Draft of text effects implementation

---
+++ Fontify/GlyphDesigner/GlyphDesigner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xwt;
+using Xwt.Drawing;
+
+namespace Fontify.GlyphDesigner
+{
+    public class GlyphDesigner
+    {
+        public GlyphDesigner ()
+        {
+            Font = Xwt.Drawing.Font.SystemFont;
+        }
+
+        public char Character { get; set; }
+
+        public Font Font { get; }
+
+        public void Render ( Context ctx )
+        {
+            ctx.SetColor ( new Color ( 1.0, 0.0, 0.0 ) );
+            ctx.SetLineWidth ( 1.0 );
+            
+            var textLayout = new TextLayout ();
+            textLayout.Font = this.Font.WithSize ( 60 );
+            textLayout.Text = Character.ToString ();
+
+            ctx.NewPath ();
+            ctx.TextLayoutPath ( textLayout, 0, 0 );
+            ctx.StrokePreserve ();
+            
+        }
+    }
+}
+
--- Fontify/GlyphDesigner/PreviewPanel.cs
+++ Fontify/GlyphDesigner/PreviewPanel.cs
@@ -1,34 +1,49 @@
 ﻿using Xwt;
 
-namespace Fontify
+namespace Fontify.GlyphDesigner
 {
-	public class PreviewPanel : VBox
-	{
-		public Slider SizeSlider;
-		public CheckBox AutoscaleCheckBox;
-		public ImageView PreviewImage;
-		public Button ConfigButton;
-		public Label PreviewLabel;
+    public class PreviewGlyphCanvas : Canvas
+    {
+        protected override void OnDraw ( Xwt.Drawing.Context ctx, Rectangle dirtyRect )
+        {
+            //base.OnDraw (ctx, dirtyRect);
+            var designer = new GlyphDesigner ();
+            designer.Character = 'A';
+            designer.Font.WithSize ( 50 ).WithFamily ( "San Serif" );
+            designer.Render ( ctx );
+        }
+    }
 
-		public PreviewPanel()
-		{
-			PreviewLabel = new Label("Preview");
-			ConfigButton = new Button("C");
+    public class PreviewPanel : VBox
+    {
+        public Slider SizeSlider;
+        public CheckBox AutoscaleCheckBox;
+        public Canvas PreviewImage;
+        public Button ConfigButton;
+        public Label PreviewLabel;
 
-			PreviewImage = new ImageView();
+        public PreviewPanel ()
+        {
+            PreviewImage = new PreviewGlyphCanvas ();
+            PreviewLabel = new Label ( "Preview" );
+            ConfigButton = new Button ( "C" );
+            /*ConfigButton.ButtonPressed += (sender, e ) =>
+            {
 
-			AutoscaleCheckBox = new CheckBox("Auto-scale");
-			SizeSlider = new HSlider();
+            };*/
 
-			var hbox1 = new HBox();
-			hbox1.PackStart(PreviewLabel, true);
-			hbox1.PackStart(ConfigButton);
+            AutoscaleCheckBox = new CheckBox ( "Auto-scale" );
+            SizeSlider = new HSlider ();
 
-			PackStart(hbox1);
-			PackStart(PreviewImage, true);
-			PackStart(AutoscaleCheckBox);
-			PackStart(SizeSlider);
-		}
-	}
+            var hbox1 = new HBox ();
+            hbox1.PackStart ( PreviewLabel, true );
+            hbox1.PackStart ( ConfigButton );
+
+            PackStart ( hbox1 );
+            PackStart ( PreviewImage, true );
+            PackStart ( AutoscaleCheckBox );
+            PackStart ( SizeSlider );
+        }
+    }
 }
 
